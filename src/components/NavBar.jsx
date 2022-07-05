1878566fbcb5bd9ae83862efe358967e00fc009b
mikemel31 <mihail3197@icloud.com>
2022-07-05 14:03:11 -0700

    added resume and boilerplate

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,8 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 import linkedin from '../assets/img/linkedin.svg.png';
 import github from '../assets/img/github.png';
 import email from '../assets/img/email.png';
-
+import resume from '../assets/img/resume.png';
+import mikeResume from '../assets/mike_melnikov_resume.pdf';
 
 export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
@@ -43,6 +44,7 @@ export const NavBar = () => {
                         <div className="social-icon">
                             <a href="https://www.linkedin.com/in/mike-mel/" target="_blank"><img src={linkedin} alt="linkedIn" height={35} width={35} /></a>
                             <a href="https://github.com/mikemel31" target="_blank"><img src={github} alt="GitHub" height={35} width={35} /></a>
+                            <a href={mikeResume} target="_blank"><img src={resume} alt="email" height={35} width={35} /></a>
                             <a href="mailto: mihail3197@gmail.com" target="_blank"><img src={email} alt="email" height={35} width={46} /></a>
                         </div>
                         {/* <button className="vvd" onClick={() => console.log('connect')}><span>Let's connect</span></button> */}
--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,6 +1,8 @@
 import linkedin from '../assets/img/linkedin.svg.png';
 import github from '../assets/img/github.png';
 import email from '../assets/img/email.png';
+import resume from '../assets/img/resume.png';
+import mikeResume from '../assets/mike_melnikov_resume.pdf';
 
 export const Social = () => {
 
@@ -8,6 +10,7 @@ export const Social = () => {
         <div className="social-icon">
             <a href="https://www.linkedin.com/in/mike-mel/" target="_blank"><img src={linkedin} alt="linkedIn" height={35} width={35} /></a>
             <a href="https://github.com/mikemel31" target="_blank"><img src={github} alt="GitHub" height={35} width={35} /></a>
+            <a href={mikeResume} target="_blank"><img src={resume} alt="email" height={35} width={35} /></a>
             <a href="mailto: mihail3197@gmail.com" target="_blank"><img src={email} alt="email" height={35} width={46} /></a>
         </div>
     )
