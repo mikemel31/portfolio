6e9dc084f4b3b52f7af36f5e051bc8a961054f76
mikemel31 <mihail3197@icloud.com>
2022-07-06 22:56:15 -0700

    Footer improvement

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -7,11 +7,13 @@ import mikeResume from '../assets/mike_melnikov_resume.pdf';
 export const Social = () => {
 
     return (
-        <div className="social-icon">
-            <a href="https://www.linkedin.com/in/mike-mel/" target="_blank"><img src={linkedin} alt="linkedIn" height={35} width={35} /></a>
-            <a href="https://github.com/mikemel31" target="_blank"><img src={github} alt="GitHub" height={35} width={35} /></a>
-            <a href={mikeResume} target="_blank"><img src={resume} alt="email" height={35} width={35} /></a>
-            <a href="mailto: mihail3197@gmail.com" target="_blank"><img src={email} alt="email" height={35} width={46} /></a>
+        <div className=" container d-flex justify-content-center border-top">
+            <footer className='justify-content-center py-3 my-2'>
+            <a className='mx-2' href="https://www.linkedin.com/in/mike-mel/" target="_blank"><img src={linkedin} alt="linkedIn" height={50} width={'auto'} /></a>
+            <a className='mx-2' href="https://github.com/mikemel31" target="_blank"><img src={github} alt="GitHub" height={50} width={"auto"} /></a>
+            <a className='mx-2' href={mikeResume} target="_blank"><img src={resume} alt="email" height={50} width={'auto'} /></a>
+            <a className='mx-0' href="mailto: mihail3197@gmail.com" target="_blank"><img src={email} alt="email" height={50} width={'auto'} /></a>
+            </footer>
         </div>
     )
 }
