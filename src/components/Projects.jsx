0225582411318e05d82dd1a1b11c8dc94d907b33
Mikhail Melnikov <mihail3197@icloud.com>
2023-01-18 12:41:50 -0800

    Reording projects and description edited. Changed favicon and title

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,29 +1,5 @@
 import { Card } from "react-bootstrap";
 
-const campArray = [
-  {
-    src: "CAMP_SCREENSHOTS/CampS1.png",
-    alt: "Main page",
-    description: "Main page",
-  },
-  {
-    src: "../CAMP_SCREENSHOTS/CampS2.png",
-    alt: "Main page (continuing)",
-    description: "Main page (continuing)",
-  },
-  {
-    src: "../CAMP_SCREENSHOTS/CampS3.png",
-    alt: "Page of Campground",
-    description: "Page of Campground",
-  },
-  {
-    src: "../CAMP_SCREENSHOTS/CampS4.png",
-    alt: "Page of Campground (with features)",
-    description: "Page of Campground (with features)",
-  },
-];
-const foodArray = [];
-
 export const Projects = () => {
   return (
     <section className="projects" id="projects">
@@ -50,7 +26,20 @@ export const Projects = () => {
                 <Card.Text>
                   This app is a copy of YelpCamp that was created during the Web
                   Developer Bootcamp. This app provides people with information
-                  about different Camps where people can stay during their trip.
+                  about different Camps where people can stay during their trip.<br/>
+                  Credentials to explore project:<br/>
+                  username: mike <br/>
+                  password: 1234
+                  <br></br>
+                  <a target="_blank" href="https://github.com/mikemel31/CampGround-project">Find out more about this project</a>
+                <ul className="my-2">Technologies used
+                  <li>EJS</li>
+                  <li>Express.js / Node.js</li>
+                  <li>MongoDB / Mongoose</li>
+                  <li>Passport</li>
+                  <li>APIs: cloudinary.com, mapbox.com</li>
+                  <li>more technologies used at <a target="_blank" href="https://github.com/mikemel31/CampGround-project">GitHub</a></li>
+                </ul>
                 </Card.Text>
               </Card.Body>
             </div>
@@ -61,7 +50,42 @@ export const Projects = () => {
         <Card style={{ minHeight: "540px" }} className="border-0">
           <div className="row">
             <div className="col-lg-7 d-inline">
-              <img className=" align-self-start img-fluid border-0"
+              <img alt="Marvel app" className=" align-self-start img-fluid border-0"
+                src="scht/MIP.png"
+                style={{transform: 'scale(0.75)'}}
+              />
+            </div>
+            <div className="col-lg-5 pt-4">
+              <Card.Body className="pt-5" style={{ minHeight: "140px" }}>
+                <Card.Title>
+                  <a href="https://mikesmarvel.herokuapp.com/">
+                    MarvelProject
+                  </a>
+                </Card.Title>
+                <Card.Text>
+                  This app made for Marvel studio fans and gives them oportunity 
+                  to know more about their favorite characters and comics. 
+                  <br></br>
+                  <a target="_blank" href="https://github.com/mikemel31/MarvelProject">Find out more about this project</a>
+                  <ul className="my-2"> Technologies used
+                    <li>React</li>
+                    <li>React Router</li>
+                    <li>Bootstrap / CSS</li>
+                    <li>Formik</li>
+                    <li>Yup</li>
+                    <li>Marvel API</li>
+                  </ul>
+                </Card.Text>
+              </Card.Body>
+            </div>
+          </div>
+        </Card>
+      </div>
+      <div className="row pt-4">
+        <Card style={{ minHeight: "540px" }} className="border-0">
+          <div className="row">
+            <div className="col-lg-7 d-inline">
+              <img alt="FoodApp" className=" align-self-start img-fluid border-0"
                 src="scht/FoodDesk.png"
                 style={{transform: 'scale(0.75)'}}
               />
@@ -76,31 +100,13 @@ export const Projects = () => {
                 <Card.Text>
                   This website was created to follow up the calorie plan that is
                   calculating on site and choose food plan for everyday delivery
-                  of precooked food.
-                </Card.Text>
-              </Card.Body>
-            </div>
-          </div>
-        </Card>
-      </div>
-      <div className="row pt-4">
-        <Card style={{ minHeight: "540px" }} className="border-0">
-          <div className="row">
-            <div className="col-lg-7 d-inline">
-              <img className=" align-self-start img-fluid border-0"
-                src="scht/MIP.png"
-                style={{transform: 'scale(0.75)'}}
-              />
-            </div>
-            <div className="col-lg-5 pt-4">
-              <Card.Body className="pt-5" style={{ minHeight: "140px" }}>
-                <Card.Title>
-                  <a href="https://mikesmarvel.herokuapp.com/">
-                    MarvelProject
-                  </a>
-                </Card.Title>
-                <Card.Text>
-                  Description is coming
+                  of precooked food. 
+                  <br></br>
+                  <a target="_blank" href="https://github.com/mikemel31/SmartFood">Find out more about this project</a>
+                  <ul>Technologies used
+                    <li>HTML / CSS / Javascript</li>
+                    <li>Webpack / Babel</li>
+                  </ul>
                 </Card.Text>
               </Card.Body>
             </div>
