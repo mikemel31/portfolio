f92b8164c5de61cd17bb4903e15615fa7c4c8281
Mikhail Melnikov <mihail3197@icloud.com>
2023-09-27 16:55:29 -0700

    updated resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,13 +13,16 @@ export const About = () => {
         <div className="col-lg-9">
           <Card.Body>
             <Card.Text className="fs-5">
-            I’m a Software Engineer with a unique background in management and
-    collaborative work experience, which sharpened my leadership and management
-    skills, especially in high-stress situations, and helped to establish and
-    maintain productive consistent working relationships with the team and
-    long-term partnerships. I am a client-service-oriented developer with a
-    vision of big picture and a sharp, analytical mindset who is highly
-    motivated by challenges and always looking to grow.
+            I’m a Software Engineer with a unique customer care background, which sharpened my leadership and
+management skills, especially in high-stress situations, and helped to establish and maintain productive
+consistent working relationships with the team and long-term partnerships. I am a client-service-oriented
+developer with a vision of the big picture and a sharp, analytical mindset who is highly motivated by
+challenges and always looking to grow.<br/>
+            </Card.Text>
+            <Card.Text className="fs-5">
+            <b>Soft skills:</b> excellent communication and team collaboration, attention to detail, ability to prioritize,
+compliance with time management, focus on results, critical thinking, emotional stability while working in
+a multitasking and fast-paced environment, self-starter, and fast learner.
             </Card.Text>
           </Card.Body>
         </div>
@@ -27,15 +30,3 @@ export const About = () => {
     </Card>
   );
 };
-{/* <div className="about">
-  <img className="myPic" src={myPic} alt="my pic" />
-  <p>
-    I’m a Software Engineer with a unique background in management and
-    collaborative work experience, which sharpened my leadership and management
-    skills, especially in high-stress situations, and helped to establish and
-    maintain productive consistent working relationships with the team and
-    long-term partnerships. I am a client-service-oriented developer with a
-    vision of big picture and a sharp, analytical mindset who is highly
-    motivated by challenges and always looking to grow.
-  </p>
-</div>; */}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -31,7 +31,7 @@ export const Projects = () => {
                   username: mike <br/>
                   password: 1234
                   <br></br>
-                  <a target="_blank" href="https://github.com/mikemel31/CampGround-project">Find out more about this project</a>
+                  <a target="_blank" href="https://github.com/mikemel31/CampGround-project">GitHub</a>
                 <ul className="my-2">Technologies used
                   <li>EJS</li>
                   <li>Express.js / Node.js</li>
@@ -40,6 +40,9 @@ export const Projects = () => {
                   <li>APIs: cloudinary.com, mapbox.com</li>
                   <li>more technologies used at <a target="_blank" href="https://github.com/mikemel31/CampGround-project">GitHub</a></li>
                 </ul>
+                <p>• Coded a user login system, storing encrypted information in cookies and SQL, to allow custom user
+experiences based on preferences while maintaining user privacy<br/>
+• Dynamically integrated 3rd party APIs: mapbox.com & cloudinary.com which helped reducing latency of the app<br/></p>
                 </Card.Text>
               </Card.Body>
             </div>
@@ -66,7 +69,7 @@ export const Projects = () => {
                   This app made for Marvel studio fans and gives them oportunity 
                   to know more about their favorite characters and comics. 
                   <br></br>
-                  <a target="_blank" href="https://github.com/mikemel31/MarvelProject">Find out more about this project</a>
+                  <a target="_blank" href="https://github.com/mikemel31/MarvelProject">GitHub</a>
                   <ul className="my-2"> Technologies used
                     <li>React</li>
                     <li>React Router</li>
@@ -75,6 +78,10 @@ export const Projects = () => {
                     <li>Yup</li>
                     <li>Marvel API</li>
                   </ul>
+                  <p>• Dynamically integrated asynchronous Axios API calls with minimization efforts to reduce latency and
+increase the app’s speed and overall smoothness by 15%<br/>
+• Converted image formats to webp to optimize quality and page load time <br/>
+• Handle app deployments by utilizing the Heroku<br/></p>
                 </Card.Text>
               </Card.Body>
             </div>
@@ -102,11 +109,15 @@ export const Projects = () => {
                   calculating on site and choose food plan for everyday delivery
                   of precooked food. 
                   <br></br>
-                  <a target="_blank" href="https://github.com/mikemel31/SmartFood">Find out more about this project</a>
+                  <a target="_blank" href="https://github.com/mikemel31/SmartFood">GitHub</a>
                   <ul>Technologies used
                     <li>HTML / CSS / Javascript</li>
                     <li>Webpack / Babel</li>
                   </ul>
+                  <p>• Used Lazy loading techniques to optimize Page Speed and user experience in general on an overall
+80%<br/>
+• Utilized the Lighthouse tool to analyze and optimize site performance, SEO, and accessibility metrics<br/>
+• Applied webpack to support different assets, configure and improve compile time by 10%<br/></p>
                 </Card.Text>
               </Card.Body>
             </div>
